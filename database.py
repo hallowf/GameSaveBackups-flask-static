89b2d40294cdc0c9b1d13356e00830bb275ffad1
hallowf <alexandre.cortegaca@gmail.com>
2018-03-13 18:52:32 +0000

    database now checks if the os is windows or linux and adds the respective path

--- database.py
+++ database.py
@@ -1,12 +1,18 @@
 import os
 import json
 from classes import Game
-
+import platform
 
 save_database = json.load(open("database.json"))
 
 def load_game(db_item):
-    [name, path] = db_item
+    current_os = platform.system
+    path = ""
+    [name, win_path, lin_path] = db_item
+    if current_os == "Windows":
+        path = win_path
+    else:
+        path = lin_path
     return Game(name, os.path.expanduser(path))
 
 save_database = map(load_game, save_database)
