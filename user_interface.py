6f0690fa1cd0e01a390cac3f089bbd0034d007a2
hallowf <alexandre.cortegaca@gmail.com>
2018-03-14 01:05:16 +0000

    exposed new function to eel that uses the correct function to check paths depending on the OS

--- user_interface.py
+++ user_interface.py
@@ -1,19 +1,26 @@
 import savebackup
 from database import save_database
 import eel
+import platform
 from search_disks import find_game
+
 
 @eel.expose
 def search_all_disks():
-    all_games = list(find_game())
-    print (all_games)
-    return all_games
-
-@eel.expose
-def get_games():
-    games_list = list(savebackup.check_game())
-    print(games_list)
-    return games_list
+    current_os = platform.system()
+    if current_os == "Windows":
+        all_games = list(find_game())
+        print (all_games)
+        return all_games
+    else:
+        games_list = list(savebackup.check_game())
+        print(games_list)
+        return games_list
+#@eel.expose
+#def get_games():
+#    games_list = list(savebackup.check_game())
+#    print(games_list)
+#    return games_list
 
 @eel.expose
 def make_backup(game):
