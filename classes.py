20a34431c849cff186cf4bf38e8627793387ec04
hallowf <alexandre.cortegaca@gmail.com>
2018-03-25 23:45:58 +0100

    Added sync paths to database updated function that searches disks to return new paths and updated the Game class

--- classes.py
+++ classes.py
@@ -1,9 +1,11 @@
 
 class Game():
-    def __init__(self, game_name, game_path):
+    def __init__(self, game_name, game_path, sync_path):
         self.name = game_name
         self.path = game_path
+        self.sync_path = sync_path
     def to_dict(self):
         self.name
         self.path
-        return {"name": self.name, "path": self.path}
+        self.sync_path
+        return {"name": self.name, "path": self.path, "sync_path": self.sync_path}
--- database.py
+++ database.py
@@ -8,13 +8,16 @@ save_database = json.load(open("database.json"))
 def load_game(db_item):
     current_os = platform.system()
     path = ""
-    [name, win_path, lin_path] = db_item
+    sync_path = ""
+    [name, win_path, win_sync, lin_path, lin_sync] = db_item
     if current_os == "Windows":
+        sync_path = win_sync
         path = win_path
-        print(path)
+        print(path, sync_path)
     else:
+        sync_path = lin_sync
         path = lin_path
-        print(path)
-    return Game(name, os.path.expanduser(path))
+        print(path, sync_path)
+    return Game(name, os.path.expanduser(path), os.path.expanduser(sync_path))
 
 save_database = map(load_game, save_database)
