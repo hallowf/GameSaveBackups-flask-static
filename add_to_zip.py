5c73755cb22fd4089362e4d98c8e0c4496047eff
hallowf <alexandre.cortegaca@gmail.com>
2018-04-07 00:25:09 +0100

    Working on function that appends files to a zip file

--- add_to_zip.py
+++ add_to_zip.py
@@ -1,13 +1,28 @@
 import zipfile
+from read_zip import read_zip_file
+from database import save_database
+from database import load_game
 
 
-def add_to_zip(zfile, game):
+def add_to_zip(zfile, sfile):
     z_file = zipfile.ZipFile(zfile, "a")
-    z_file.write(game)
+    z_file.write(sfile)
     z_file.close()
-    z_file = zipfile.ZipFile(zfile)
-    z_file.printdir()
 
 
 
-add_to_zip("zippedBackups.zip", "Game")
+def append_to_zip(zfile, sfile):
+
+
+
+
+
+
+def try_reading():
+    for game in save_database:
+        game_name = "" 
+        game_dict = game.to_dict()
+        if "name" in game_dict:
+            game_name = game_dict["name"]
+            print(game_name)
+        
