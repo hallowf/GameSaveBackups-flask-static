d864f3dee1a94cc6304a260625edcc5b212b561b
hallowf <alexandre.cortegaca@gmail.com>
2018-05-13 23:53:46 +0100

    Corrected imports on getAll_from_database

--- getAll_from_database.py
+++ getAll_from_database.py
@@ -1,8 +1,8 @@
-from database import save_database
+from Database import database
 
 
 def generated_games():
     g = []
-    for game in save_database:
+    for game in database.save_database:
         g.append(game.to_dict())
     return g
