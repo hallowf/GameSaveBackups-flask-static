fe6a056d4ed6b1b7bfd42ec56a0b26757c166808
hallowf <alexandre.cortegaca@gmail.com>
2018-05-13 15:30:55 +0100

    Added new entry to test

--- test_test1.py
+++ test_test1.py
@@ -5,3 +5,6 @@ import steam_id
 def test_check_converted_id():
     converted_user_id = steam_id.convert_id("STEAM_0:1:35807358")
     assert converted_user_id == '71614717'
+
+
+#def test_check_conversion_of_paths():
