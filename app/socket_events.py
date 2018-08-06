99587072f4eed43dd4e04076dfda5ee5aa816867
hallowf <alexandre.cortegaca@gmail.com>
2018-08-07 00:49:35 +0100

    Added socket functionality

---
+++ app/socket_events.py
@@ -0,0 +1,19 @@
+from flask_socketio import SocketIO, emit
+from app import socketio
+
+### Socket events
+@socketio.on('my event', namespace='/test')
+def test_message(message):
+    emit('my response', {'data': message['data']})
+
+@socketio.on('my broadcast event', namespace='/test')
+def test_message(message):
+    emit('my response', {'data': message['data']}, broadcast=True)
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    emit('my response', {'data': 'Connected'})
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
