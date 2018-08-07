db17a5d088b10da0cd1bb77f128ca026b65285e6
hallowf <alexandre.cortegaca@gmail.com>
2018-08-07 18:27:17 +0100

    Updated names on events

--- app/socket_events.py
+++ app/socket_events.py
@@ -2,18 +2,19 @@ from flask_socketio import SocketIO, emit
 from app import socketio
 
 ### Socket events
-@socketio.on('my event', namespace='/test')
+@socketio.on("event listener", namespace="/websocket")
 def test_message(message):
-    emit('my response', {'data': message['data']})
+    print(message["data"])
+    emit("my response", {"data": message["data"]})
 
-@socketio.on('my broadcast event', namespace='/test')
+@socketio.on("broadcast event", namespace="/websocket")
 def test_message(message):
-    emit('my response', {'data': message['data']}, broadcast=True)
+    emit("my response", {"data": message["data"]}, broadcast=True)
 
-@socketio.on('connect', namespace='/test')
+@socketio.on("connect", namespace="/websocket")
 def test_connect():
-    emit('my response', {'data': 'Connected'})
+    emit("my response", {"data": "Connected"})
 
-@socketio.on('disconnect', namespace='/test')
+@socketio.on("disconnect", namespace="/websocket")
 def test_disconnect():
-    print('Client disconnected')
+    print("Client disconnected")
