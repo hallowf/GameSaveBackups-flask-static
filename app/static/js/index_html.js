650a30f10953a3e3c5d14b378b7fc18d4c073450
hallowf <alexandre.cortegaca@gmail.com>
2018-08-08 23:00:35 +0100

    working on template rendering

--- app/static/js/index_html.js
+++ app/static/js/index_html.js
@@ -1,5 +1,37 @@
+// Template for creating game cards
+const cardTemplate = _.template(`
+  <div class="card bg-secondary game_cards" style="width:85%; background-color:#eae8ea; margin:1em auto">
+    <img class="card-img-top" src="/img/gameimgs/<%=name%>.jpg" alt="Card image cap">
+    <div class="card-body">
+      <h5 class="card-title"><%=name%></h5>
+      <p class="card-text"><%=path%></p>
+      <p class="card-text"><%=sync_path%></p>
+      <label class="btn btn-secondary active">
+        <input class ="checkbox" style="margin:1em auto" type="checkbox" autocomplete="off"> Backup
+      </label>
+      <label class="btn btn-secondary active">
+        <input class ="steamCloud" style="margin:1em auto" type="checkbox" autocomplete="off"> Steam Cloud
+      </label>
+    </div>
+  </div>
+`)
 
+// Websocket connection
 const socket = io.connect("http://" + document.domain + ":" + location.port + "/websocket")
+
+
+function renderGames(games) {
+  const the_row = document.getElementById("the_row")
+  the_row.innerHTML = ""
+  games.forEach(game => {
+    var element = $('<div></div>').addClass('col-sm-3')
+    element.html(cardTemplate({ "name": JSON.stringify(game.name), "path": JSON.stringify(game.path), "sync_path": JSON.stringify(game.sync_path)}))
+    the_row.append(element)
+  })
+}
+
+
+
 
 function searchUnsynced() {
   const unsyncedButton = document.getElementById("no_sync")
@@ -11,6 +43,7 @@ function searchUnsynced() {
     gameList.forEach(function(object){
       console.log("Received: " + JSON.stringify(object))
     })
+    renderGames(msg.dict)
   })
 }
 
@@ -22,12 +55,10 @@ function searchSynced() {
   })
   socket.on("synced_game_list", function(msg) {
     const syncedGameList = msg.dict
-    for (var i = 0; i < syncedGameList.length; i++) {
-      console.log("Found: " + JSON.stringify(syncedGameList[i]))
-    }
     syncedGameList.forEach(function(object) {
       console.log("Received: " + JSON.stringify(object))
     })
+    renderGames(msg.dict)
   })
 }
 
