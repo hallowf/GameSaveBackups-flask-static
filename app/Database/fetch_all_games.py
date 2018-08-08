236c2953b3a83e632ebfebecf4d659b50a60eba3
hallowf <alexandre.cortegaca@gmail.com>
2018-08-08 01:30:51 +0100

    Merged search functionality and added comments

--- app/Database/fetch_all_games.py
+++ app/Database/fetch_all_games.py
@@ -2,6 +2,14 @@ import os
 import json
 import platform
 
+### Check OS
+current_os = platform.system()
+
+if current_os.upper() == "WINDOWS":
+    import win32api
+else:
+    pass
+
 
 ### class for constructing game objects
 
@@ -17,12 +25,8 @@ class Game():
         return {"name": self.name, "path": self.path, "sync_path": self.sync_path}
 
 
-
-
 ### The game database to load
-save_database = json.load(open("app\\Database\\database.json"))
-
-
+save_database = json.load(open("app/Database/database.json"))
 
 
 ### Function to return game objects
@@ -34,20 +38,53 @@ def load_game(db_item):
     if current_os == "Windows":
         sync_path = win_sync
         path = win_path
-        print(path, sync_path)
+        #print(path, sync_path)
     else:
         sync_path = lin_sync
         path = lin_path
-        print(path, sync_path)
-    print(classes.Game(name, os.path.expanduser(path), os.path.expanduser(sync_path)))
-    return classes.Game(name, os.path.expanduser(path), os.path.expanduser(sync_path))
+        #print(path, sync_path)
+    #print(Game(name, os.path.expanduser(path), os.path.expanduser(sync_path)))
+    return Game(name, os.path.expanduser(path), os.path.expanduser(sync_path))
 
-## Assign to save_database a list with each game and it's attributes from the database
 
+### Assign to save_database a list with each game and it's attributes from the database
 save_database = list(map(load_game, save_database))
 
+
+
+### Functions to yield games to dict
+
+## Simple function that simply returns all games
 def generate_games():
     g = []
     for game in save_database:
         g.append(game.to_dict())
     return g
+
+
+### Check the OS and searches for the games in steam library's across all hard drives
+## TODO: At the moment it can only search all drives in windows, add linux functionality
+def convert_path(user_id):
+    if current_os == "Windows":
+        for game in save_database:
+            new_sync = game.sync_path.replace("XXXXX", str(user_id))
+            drive_letters = win32api.GetLogicalDriveStrings().split("\000")[:-1]
+            for drive in drive_letters:
+                if os.path.isdir(new_sync):
+                    game.sync_path = new_sync
+                    yield game.to_dict()
+                else:
+                    new_path = new_sync.replace("C:\\", drive)
+                    if os.path.isdir(new_path):
+                        game.sync_path = new_path
+                        yield game.to_dict()
+                    else:
+                        print("Couldn't find " + game.name + " at " + new_path)
+    else:
+        for game in save_database:
+            new_sync = game.sync_path.replace("XXXXX", str(user_id))
+            if os.path.isdir(new_sync):
+                print("Found game at:" + new_sync)
+                yield game.to_dict()
+            else:
+                print ("Couldn't find " + game.name + " at " + new_sync)
