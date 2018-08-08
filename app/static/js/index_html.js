bbb1cb35a3cdbe62d69565a7937a8baf5f6e25b4
hallowf <alexandre.cortegaca@gmail.com>
2018-08-08 16:02:46 +0100

    Updated namings and added logs for incoming arrays

--- app/static/js/index_html.js
+++ app/static/js/index_html.js
@@ -1,20 +1,35 @@
 
-
-var socket = io.connect('http://' + document.domain + ':' + location.port + '/websocket')
-
+const socket = io.connect("http://" + document.domain + ":" + location.port + "/websocket")
 
 function searchUnsynced() {
   const unsyncedButton = document.getElementById("no_sync")
   unsyncedButton.onclick = (function() {
-    socket.emit('event listener', {data: "search_unsynced"})
+    socket.emit("data request", {request: "search_unsynced"})
   })
-  socket.on('my response', function(msg) {
-    console.log('Received: ' + msg.data)
+  socket.on("unsynced_game_list", function(msg) {
+    const gameList = msg.dict
+    gameList.forEach(function(object){
+      console.log("Received: " + JSON.stringify(object))
+    })
   })
 }
 
 function searchSynced() {
-  console.log("Somebody")
+  const syncedButton = document.getElementById("steam_sync")
+  syncedButton.onclick = (function() {
+    let idInput = document.getElementById("user_id").value
+    socket.emit("data request", {request: "search_synced " + idInput})
+  })
+  socket.on("synced_game_list", function(msg) {
+    const syncedGameList = msg.dict
+    for (var i = 0; i < syncedGameList.length; i++) {
+      console.log("Found: " + JSON.stringify(syncedGameList[i]))
+    }
+    syncedGameList.forEach(function(object) {
+      console.log("Received: " + JSON.stringify(object))
+    })
+  })
 }
 
 searchUnsynced()
+searchSynced()
