96efc39d17c8e3877e8a5dc84558234eb664fffa
hallowf <alexandre.cortegaca@gmail.com>
2018-08-08 16:02:10 +0100

    Updated names and added socket functionality to send game arrays to client-side

--- app/socket_events.py
+++ app/socket_events.py
@@ -1,19 +1,28 @@
 from flask_socketio import SocketIO, emit
 from app import socketio
+from app.Database.fetch_all_games import generate_games, convert_path
 
 ### Socket events
 @socketio.on("event listener", namespace="/websocket")
 def test_message(message):
-    print(message["data"])
-    emit("my response", {"data": message["data"]})
+    emit("response", {"data": message["data"]})
+
+@socketio.on("data request", namespace="/websocket")
+def send_data(message):
+    print(message["request"][:13])
+    if message["request"] == "search_unsynced":
+        emit("unsynced_game_list", {"dict": generate_games()})
+    elif message["request"][:13] == "search_synced":
+        user_id = message["request"].replace("search_synced ", "")
+        emit("synced_game_list", {"dict": convert_path(user_id)})
 
 @socketio.on("broadcast event", namespace="/websocket")
 def test_message(message):
-    emit("my response", {"data": message["data"]}, broadcast=True)
+    emit("response", {"data": message["data"]}, broadcast=True)
 
 @socketio.on("connect", namespace="/websocket")
 def test_connect():
-    emit("my response", {"data": "Connected"})
+    emit("response", {"data": "Connected"})
 
 @socketio.on("disconnect", namespace="/websocket")
 def test_disconnect():
