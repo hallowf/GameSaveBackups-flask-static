7386ddb81ac729ae343e6e788f48d6557579c8f2
hallowf <alexandre.cortegaca@gmail.com>
2018-08-09 16:37:23 +0100

    Updated generate games if statement, comment typo, and removed unnecessary print

--- app/Database/fetch_all_games.py
+++ app/Database/fetch_all_games.py
@@ -30,6 +30,8 @@ class Game():
 ### The game database to load
 save_database = json.load(open("app/Database/database.json"))
 
+#save_database = json.load(open("database.json"))
+
 
 ### Function to return game objects
 def load_game(db_item):
@@ -40,12 +42,9 @@ def load_game(db_item):
     if current_os == "Windows":
         sync_path = win_sync
         path = win_path
-        #print(path, sync_path)
     else:
         sync_path = lin_sync
         path = lin_path
-        #print(path, sync_path)
-    #print(Game(name, os.path.expanduser(path), os.path.expanduser(sync_path)))
     return Game(name, os.path.expanduser(path), os.path.expanduser(sync_path))
 
 
@@ -56,15 +55,16 @@ save_database = list(map(load_game, save_database))
 
 ### Functions to yield games to dict
 
-## Simple function that simply returns all games
+## Simple function that returns all games
 ## Wont work for steam sync
 def generate_games():
     g = []
     for game in save_database:
-        if os.path.isdir(game.path):
+        if os.path.isdir(game.path) and game.found == False:
             g.append(game.to_dict())
+            game.found = True
         else:
-            print("Games does not exist")
+            continue
     return g
 
 
