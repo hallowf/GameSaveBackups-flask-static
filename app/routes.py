80be88a7c096744c3ddb3f082977347b9d18756b
hallowf <alexandre.cortegaca@gmail.com>
2018-08-12 19:34:40 +0100

    Working on api route

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,23 @@
-from flask import Flask, render_template, Response, send_file, request
+from flask import Flask, render_template, Response, send_file, request, jsonify
 from app import app
+from app.Database.fetch_all_games import convert_path as get_games, generate_games
 
 ### Page routes
 
 @app.before_request
 def before_req():
     #request.url
+    pass
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/api/games")
+def games():
+    #import pdb; pdb.set_trace()
+    user_id = request.args['user_id']
+    return jsonify({
+        "synced_game_list": get_games(user_id),
+        "unsynced_game_list": generate_games(),
+    })
