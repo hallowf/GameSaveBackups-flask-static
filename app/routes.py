a616fc6c48d269031c65fd88296b00ab4a9e2649
hallowf <alexandre.cortegaca@gmail.com>
2018-08-12 17:33:21 +0100

    Updated gitignore working on flask routes

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, Response, send_file
+from flask import Flask, render_template, Response, send_file, request
 from app import app
 
 ### Page routes
+
+@app.before_request
+def before_req():
+    #request.url
 
 @app.route("/")
 def index():
