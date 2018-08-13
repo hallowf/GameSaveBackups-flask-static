dbd391c16668df32abf85451b9c8ee9167600317
hallowf <alexandre.cortegaca@gmail.com>
2018-08-13 19:02:06 +0100

    Changed flask_server ip for heroku deployment

--- flask_server.py
+++ flask_server.py
@@ -10,4 +10,4 @@ app.config['SECRET_KEY'] = 'secret!'
 # Run app
 
 if __name__ == "__main__":
-    socketio.run(app, host="127.0.0.1",port=2890, debug=True)
+    socketio.run(app, host="0.0.0.0",port=2890, debug=True)
